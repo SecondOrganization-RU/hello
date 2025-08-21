2d6b5f4936854fe45f0dc854bf26ac3cd11c2f13
Rakesh Upadhye <rakesh.upadhye@sonarsource.com>
2025-08-21 10:35:34 +0200

    Initial commit

---
+++ hello.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
+
+func myFunction(x int, y int) int {
+	
+	//This is a test function
+	//to validate the comments and its problem on SQC. 
+	//
+	//
+	//
+	//
+	///
+	///
+	//
+	//
+	//
+	
+	
+  return x + y
+}
